8ca592d9d06ae9f7cd1a09b13a19272405dcd22c
Huynhnhu169 <158531418+Huynhnhu169@users.noreply.github.com>
2024-10-29 11:53:03 +0700

    Add files via upload

---
+++ colors.py
@@ -0,0 +1,51 @@
+GRID_COLOR = '#a39489'
+EMPTY_CELL_COLOR = '#c2b3a9'
+SCORE_LABEL_COLOR = '#92967f'
+SCORE_LABEL_FONT = ('Helvetica', 24)
+SCORE_FONT = ('Helvetica', 36, 'bold')
+GAME_OVER_FONT = ('Helvetica', 48, 'bold')
+GAME_OVER_FONT_COLOR = '#ffffff'
+WINNER_BG = '#ffcc00'
+LOSER_BG = '#a39489'
+
+CELL_COLORS = {
+    2: '#eee4da',
+    4: '#ede0c8',
+    8: '#f2b179',
+    16: '#f59563',
+    32: '#f67c5f',
+    64: '#f65e3b',
+    128: '#edcf72',
+    256: '#edcc61',
+    512: '#edc850',
+    1024: '#edc53f',
+    2048: '#edc22e',
+}
+
+CELL_NUMBER_COLORS = {
+    2: '#776e65',
+    4: '#776e65',
+    8: '#f9f6f2',
+    16: '#f9f6f2',
+    32: '#f9f6f2',
+    64: '#f9f6f2',
+    128: '#f9f6f2',
+    256: '#f9f6f2',
+    512: '#f9f6f2',
+    1024: '#f9f6f2',
+    2048: '#f9f6f2',
+}
+
+CELL_NUMBER_FONTS = {
+    2: ('Helvetica', 55, 'bold'),
+    4: ('Helvetica', 55, 'bold'),
+    8: ('Helvetica', 55, 'bold'),
+    16: ('Helvetica', 50, 'bold'),
+    32: ('Helvetica', 50, 'bold'),
+    64: ('Helvetica', 50, 'bold'),
+    128: ('Helvetica', 45, 'bold'),
+    256: ('Helvetica', 45, 'bold'),
+    512: ('Helvetica', 45, 'bold'),
+    1024: ('Helvetica', 40, 'bold'),
+    2048: ('Helvetica', 40, 'bold'),
+}
